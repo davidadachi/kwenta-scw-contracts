858598cc738b356de161682f9d7545f3cd8ef294
David Adachi <davidadachi56@gmail.com>
2023-09-28 13:02:37 -0400

    🗑️ Remove import

--- src/SMv2SessionValidationModule.sol
+++ src/SMv2SessionValidationModule.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
-import {IAccount} from "src/kwenta/smv2/IAccount.sol";
 import {
     ISessionValidationModule,
     UserOperation
